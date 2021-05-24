14ed191ca88c47e8518162dce57247b3505e5889
StellarFX <priveeofficiel@gmail.com>
2021-05-24 13:17:21 +0200

    Modified index.js

--- static/index.js
+++ static/index.js
@@ -1,6 +1,28 @@
-var socket = io();
+$(() => {
 
-socket.on('connect', () => {
-  var name = window.prompt("Enter your name: ");
-  socket.emit('name', {data: name});
+  var socket = io();
+
+  $("#form").on('submit', (e) => {
+
+      e.preventDefault();
+
+      var username = $('#username').val();
+      console.log(username);
+
+      return socket.emit('name', { data: username });
+
+  })
+  
+  socket.on('userConnect', () => {
+      var userConnect = document.createElement('div');
+      userConnect.innerHTML = "<p>Un utilisateur s'est connecté</p>";
+      $("#connectlist").append(userConnect);
+  })
+
+  socket.on('userDisconnect', () => {
+      var userDisconnect = document.createElement('div');
+      userDisconnect.innerHTML = "<p>Un utilisateur s'est déconnecté</p>";
+      $("#connectlist").append(userDisconnect);
+  })
+
 })
